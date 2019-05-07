78b9224d2def7f495758b042b37d95cd754f4a4a
tomitomi3 <tomi.nori+github@gmail.com>
2019-05-08 01:08:50 +0900

    UARTテストプログラム

---
+++ CommunicationArduino/CommunicationArduino/Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.tbxPort = New System.Windows.Forms.TextBox()
+        Me.tbxSendMessage = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(118, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Open"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'tbxPort
+        '
+        Me.tbxPort.Location = New System.Drawing.Point(12, 12)
+        Me.tbxPort.Name = "tbxPort"
+        Me.tbxPort.Size = New System.Drawing.Size(100, 19)
+        Me.tbxPort.TabIndex = 1
+        '
+        'tbxSendMessage
+        '
+        Me.tbxSendMessage.Location = New System.Drawing.Point(12, 49)
+        Me.tbxSendMessage.Name = "tbxSendMessage"
+        Me.tbxSendMessage.Size = New System.Drawing.Size(100, 19)
+        Me.tbxSendMessage.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(118, 47)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Send"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(319, 167)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.tbxSendMessage)
+        Me.Controls.Add(Me.tbxPort)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "frmMain"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents tbxPort As TextBox
+    Friend WithEvents tbxSendMessage As TextBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ CommunicationArduino/CommunicationArduino/Form1.vb
@@ -0,0 +1,121 @@
+﻿Imports System.IO.Ports
+Imports System.Threading
+
+Public Class frmMain
+    Declare Function AllocConsole Lib "kernel32" () As Int32
+    Private oSerialPort As SerialPort = Nothing
+    Private myThread As Thread = Nothing
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AllocConsole()
+        oSerialPort = New SerialPort()
+        oSerialPort.BaudRate = 9600
+        oSerialPort.StopBits = StopBits.One
+        oSerialPort.RtsEnable = False
+        oSerialPort.DataBits = 8
+        oSerialPort.Parity = False
+
+        Dim ports = System.IO.Ports.SerialPort.GetPortNames()
+        For Each portName In ports
+            Console.WriteLine("{0}", portName)
+        Next
+        If ports.Length <> 0 Then
+            Me.tbxPort.Text = String.Format("{0}", ports(0))
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If oSerialPort.IsOpen Then
+            'thread stop
+            myThread.Abort()
+
+            'close
+            oSerialPort.Close()
+        End If
+
+        Try
+            Me.oSerialPort.PortName = Me.tbxPort.Text
+            Me.oSerialPort.Open()
+
+            'thread start
+            Me.myThread = New Thread(AddressOf threadFunc)
+            Me.myThread.IsBackground = True
+            Thread.Sleep(500)
+            Me.myThread.Start()
+            Thread.Sleep(500)
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private _lock = New Object
+    Private recvData As New List(Of Byte)
+    Private Const ReadBufferSize As Integer = 32
+
+    Private Sub threadFunc()
+        Dim recentReadCount As Integer = 10
+        recvData.Clear()
+
+        While (True)
+            'バッファサイズのオーバー疑い
+            If Me.oSerialPort.BytesToRead > (Me.oSerialPort.ReadBufferSize / 2) Then
+                Exit While
+            End If
+
+            'データ読み込み
+            SyncLock (_lock)
+                Dim isRead As Boolean = True
+                Dim temp() As Byte = Nothing
+                Dim readSize As Integer = 0
+                Dim nowReadBufferSize As Integer = Me.oSerialPort.BytesToRead
+                If nowReadBufferSize = 0 Then
+                    isRead = False
+                ElseIf nowReadBufferSize > ReadBufferSize Then
+                    '指定したサイズ分をバッファから読み込む
+                    ReDim temp(ReadBufferSize - 1)
+                    readSize = ReadBufferSize
+                ElseIf recentReadCount > 10 Then
+                    'バッファサイズに変更がない＝バッファをすべて空にする
+                    recentReadCount = 0
+                    ReDim temp(nowReadBufferSize - 1)
+                    readSize = nowReadBufferSize
+                Else
+                    recentReadCount += 1
+                    isRead = False
+                End If
+
+                '受信データをデシリアライズ
+                If isRead = True Then
+                    Console.WriteLine(Me.oSerialPort.BytesToRead)
+
+                    'read
+                    For i As Integer = 0 To readSize - 1
+                        i += oSerialPort.Read(temp, 0, readSize - i)
+                    Next
+
+                    Console.WriteLine(Me.oSerialPort.BytesToRead)
+                    For Each value In temp
+                        recvData.Add(value)
+                    Next
+
+                    Console.WriteLine("Size:{0}", recvData.Count)
+                    For Each tempValue In recvData
+                        'Console.WriteLine("{0}", tempValue)
+                    Next
+                    recvData.Clear()
+                End If
+            End SyncLock
+
+            Thread.Sleep(50)
+        End While
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If oSerialPort.IsOpen = False Then
+            Return
+        End If
+
+
+    End Sub
+End Class
